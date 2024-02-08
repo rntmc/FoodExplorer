7719ef7314b30a03fa0769dca8b0813bf748ba14
renato <renatocarrino@gmail.com>
2024-02-08 17:28:07 -0300

    signIn e SignUp finalizados para mobile

--- front-end/src/pages/SignIn/styles.js
+++ front-end/src/pages/SignIn/styles.js
@@ -1,55 +1,77 @@
 import styled from 'styled-components';
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints';
 
 export const Container = styled.div`
   height: 100vh;
+  width: 100vw;
 
   display: flex;
-  align-items: stretch;
   justify-content: center;
-  
+  align-items: center;
+
+  @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+    display:grid;
+    grid-template-areas:
+    "logo"
+    "form";
+  }
 `;
 
 export const Form = styled.form`
-  width: 476px;
-  height: 540px;
+  width: 29.75rem;
+  height: 34rem;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  margin-right: 140px;
-  margin-top: 100px;
-  
+  justify-content: start;
+  margin-right: 8.75rem;
   background-color: ${({theme}) => theme.COLORS.DARK_700};
   border-radius: 16px;
-  padding: 64px;
+  padding: 4rem;
 
+  @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+    grid-area:form;
+    margin-right:0;
+    margin-top: -8rem;
+    margin-right: 0;
+    background-color:transparent;
+    padding: 0 4rem;
+  }
 
   > h1 {
-    font-family: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM}
-    font-size: 32px;
+    font-family: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM.fontFamily};
+    font-size: 2rem;
     font-weight:400;
-    margin-bottom: 16px;
+    margin-bottom: 1rem;
+
+    @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+      display:none;
+    }
   };
 
   > p {
-    font-family: ${({ theme }) => theme.FONTS.ROBOTO_SMALL_REGULAR};
-    font-size: 16px;
+    font-family: ${({ theme }) => theme.FONTS.ROBOTO_SMALL_REGULAR.fontFamily};
+    font-size: 1rem;
     font-weight: 300;
     align-self:flex-start;
-    margin-bottom: 8px;
-    margin-top: 16px;
+    margin-bottom: 0.5rem;
+    margin-top: 1rem;
   }
 
   > a {
     text-decoration: none;
-    margin-top: 32px;
+    margin-top: 2rem;
     color: ${({theme}) => theme.COLORS.LIGHT_100};
     font-size: ${({ theme }) => theme.FONTS.POPPINS_100_MEDIUM.fontSize};
   }
-  
+
+  > Input {
+    padding: 0;
+  }
+
   > Button {
-    margin-top: 32px;
+    margin-top: 2rem;
   }
 `;
 
@@ -58,16 +80,25 @@ export const Logo = styled.div`
   flex: 1;
   justify-content: center;
   align-items: center;
-  gap: 20px;
+  gap: 1.25rem;
+
+  @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+    grid-area:logo;
+    display:flex;
+    align-items:center;
+    justify-content:start;
+    margin-top:5rem;
+    padding: 0 4rem;
+  }
 
   > svg {
-    font-size: 50px;
+    font-size: 3.125rem;
     color: ${({theme}) => theme.COLORS.CAKE_100};
   }
 
   > h2 {
       font-family: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontFamily};
-      font-size: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontSize};
-      font-weight: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontWeight};
+      font-size: 2.62rem;
+      font-weight: bold;
   }
 `;
--- front-end/src/pages/SignUp/styles.js
+++ front-end/src/pages/SignUp/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints';
 
 export const Container = styled.div`
   height: 100vh;
@@ -7,36 +8,60 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+    display:grid;
+    grid-template-areas:
+    "logo"
+    "form";
+  }
 `;
 
 export const Form = styled.form`
-  width: 476px;
-  height: 620px;
+  width: 29.75rem;
+  height: 38.75rem;
 
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  margin-right: 140px;
+  justify-content: start;
+  margin-right: 8.75rem;
   background-color: ${({theme}) => theme.COLORS.DARK_700};
   border-radius: 16px;
-  padding: 64px;
+  padding: 4rem;
+
+  @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+    grid-area:form;
+    margin-right:0;
+    margin-top: -8rem;
+    margin-right: 0;
+    background-color:transparent;
+    padding: 0 4rem;
+  }
 
   > h1 {
-    font-size: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM.fontSize};
-    margin-bottom: 16px;
+    font-family: ${({ theme }) => theme.FONTS.POPPINS_400_MEDIUM.fontFamily};
+    font-size: 2rem;
+    font-weight:400;
+    margin-bottom: 1rem;
+
+    @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+      display:none;
+    }
   };
 
   > p {
-    font-size: ${({ theme }) => theme.FONTS.ROBOTO_SMALL_REGULAR.fontSize};
+    font-family: ${({ theme }) => theme.FONTS.ROBOTO_SMALL_REGULAR.fontFamily};
+    font-size: 1rem;
+    font-weight: 300;
     align-self:flex-start;
-    margin-bottom: 8px;
-    margin-top: 16px;
+    margin-bottom: 0.5rem;
+    margin-top: 1rem;
   }
 
   > a {
     text-decoration: none;
-    margin-top: 32px;
+    margin-top: 2rem;
     color: ${({theme}) => theme.COLORS.LIGHT_100};
     font-size: ${({ theme }) => theme.FONTS.POPPINS_100_MEDIUM.fontSize};
   }
@@ -46,7 +71,7 @@ export const Form = styled.form`
   }
 
   > Button {
-    margin-top: 32px;
+    margin-top: 2rem;
   }
 `;
 
@@ -55,16 +80,26 @@ export const Logo = styled.div`
   flex: 1;
   justify-content: center;
   align-items: center;
-  gap: 20px;
+  gap: 1.25rem;
+
+  @media(max-width:${DEVICE_BREAKPOINTS.MD}){
+    grid-area:logo;
+    display:flex;
+    align-items:center;
+    justify-content:start;
+    margin-top:5rem;
+    padding: 0 4rem;
+    margin-bottom:4rem;
+  }
 
   > svg {
-    font-size: 50px;
+    font-size: 3.125rem;
     color: ${({theme}) => theme.COLORS.CAKE_100};
   }
 
   > h2 {
       font-family: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontFamily};
-      font-size: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontSize};
-      font-weight: ${({ theme }) => theme.FONTS.ROBOTO_GIANT_BOLD.fontWeight};
+      font-size: 2.62rem;
+      font-weight: bold;
   }
 `;
